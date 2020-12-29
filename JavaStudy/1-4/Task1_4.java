51c611475545b71af35e89df8f01c7aa19661ae0
Kenichi Yamagami <75458805+kenchan828@users.noreply.github.com>
2020-12-29 13:21:01 +0900

    Update Task1_4.java

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -14,7 +14,7 @@ public class Task1_4 {
     // 定数（アカウント情報）
     // ① 登録されている名前とパスワードを定数で定義してください。
         final String name = "alice";
-        final String name = "pass";
+        final String pass = "pass";
     // 定数（メッセージ）
     private static final String CONST_MSG_SUCCESS = "ログイン成功です。";
     private static final String CONST_MSG_ERROR_NAME = "名前に誤りがあります。";
@@ -33,13 +33,13 @@ public class Task1_4 {
          String name = "alice";
          String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
-        if  ( String name.equals("alice") && pass.equals("pass") ); {
+        if  ( name.equals("alice") && pass.equals("pass") ); {
             System.out.println("ログイン成功です");
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-        }elseif (String name.equals("alice") ); {
+        }elseif (name.equals("alice") ); {
             System.out.println("パスワードに誤りがあります");
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-        }elseif (String pass.equals("pass") ); {
+        }elseif (pass.equals("pass") ); {
             System.out.println("名前に誤りがあります");
         // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
         }else {
