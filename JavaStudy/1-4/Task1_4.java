2a90a2a66b06cd71efbbc4e01cfdfefda0a7428d
Kenichi Yamagami <75458805+kenchan828@users.noreply.github.com>
2021-01-01 10:38:02 +0900

    Update Task1_4.java

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -33,13 +33,13 @@ public class Task1_4 {
          String name = "alice";
          String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
-        if  ( name.equals("alice") && pass.equals("pass") ); {
+        if  ( name.equals("alice") && pass.equals("pass") ){
             System.out.println("ログイン成功です");
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-        }elseif (name.equals("alice") ); {
+        }elseif (name.equals("alice") ) {
             System.out.println("パスワードに誤りがあります");
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-        }elseif (pass.equals("pass") ); {
+        }elseif (pass.equals("pass") ) {
             System.out.println("名前に誤りがあります");
         // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
         }else {
