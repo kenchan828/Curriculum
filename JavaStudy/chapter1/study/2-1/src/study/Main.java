de32d19ca96df6be72fc7d87eca5816f972e37f4
Kenichi Yamagami <75458805+kenchan828@users.noreply.github.com>
2021-01-17 00:03:24 +0900

    2-1

--- JavaStudy/chapter1/study/2-1/src/study/Main.java
+++ JavaStudy/chapter1/study/2-1/src/study/Main.java
@@ -18,7 +18,7 @@ public class Main {
     		String lastName = "yamagami";
 
         //getName関数の呼び出しと出力
-        System.out.println(getName(firstName, lastName));
+        System.out.println(getName(firstName,lastName));
         System.out.println();
 
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -26,7 +26,7 @@ public class Main {
         //③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
         //   要素が奇数の場合には「nは奇数です。」と出力されます。
         	for (int num =0; num <arr.length; num++) {
-        		isOdd(int[num] arr);
+        		isOdd(arr[num]);
         	}
 
 
@@ -37,7 +37,7 @@ public class Main {
     //  修飾子：public
     //   引数には①で定義したfirstNameとlastNameを引数で受け取り、
     //  last_name と first_name を連結した値を返り値とする変数を定義しなさい。
-    	public  static getName(Str firstName, Str lastName) {
+    	public  static String getName(String firstName, String lastName) {
     		return(firstName + lastName);
     	}
 
